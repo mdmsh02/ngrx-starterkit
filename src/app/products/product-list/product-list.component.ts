653300f2165ae2e407629aff553f993b688905b7
Saif <MohammedSaif.Husain@utc.com>
2018-07-21 15:15:51 +0530

    refactor: use selector to select slice of state and compose them

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -41,12 +41,8 @@ export class ProductListComponent implements OnInit, OnDestroy {
         (err: any) => (this.errorMessage = err.error)
       );
       this.store
-      .pipe(select('products'))
-      .subscribe(products => {
-        if (products) {
-          this.displayCode = products.showProductCode;
-        }
-      });
+      .pipe(select(fromState.getShowProductCode))
+      .subscribe(showProductCode => this.displayCode = showProductCode);
   }
 
   ngOnDestroy(): void {
--- src/app/products/state/product.reducer.ts
+++ src/app/products/state/product.reducer.ts
@@ -1,5 +1,6 @@
 import { Product } from '../product';
 import * as fromRoot from '../../state/app.state';
+import { createFeatureSelector, createSelector } from '@ngrx/store';
 
 export interface State extends fromRoot.State {
     products: ProductState;
@@ -9,8 +10,28 @@ export interface ProductState {
     currentProduct: Product;
     products: Product[];
 }
+const initialState: ProductState = {
+    showProductCode: true,
+    currentProduct: null,
+    products: []
+};
 
-export function reducer (state: ProductState, action): ProductState {
+const getProductFeatureSelector = createFeatureSelector<ProductState>('products');
+
+export const getShowProductCode = createSelector(
+    getProductFeatureSelector, state => state.showProductCode);
+
+export const getCurrentProduct = createSelector(
+    getProductFeatureSelector,
+    state => state.currentProduct
+);
+
+export const getProducts = createSelector(
+    getProductFeatureSelector,
+    state => state.products
+);
+
+export function reducer(state = initialState, action): ProductState {
     switch (action.type) {
         case 'TOOGLE_PRODUCT_CODE':
             return {
