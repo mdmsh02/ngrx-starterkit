a3fdda389c4404d353a89c98b9a9d05abc1664bd
Saif-Husain <Saif-Hussain@epam.com>
2018-07-22 12:32:06 +0530

    add action and enum for update product

--- src/app/products/state/product.action.ts
+++ src/app/products/state/product.action.ts
@@ -8,7 +8,10 @@ export enum ProductActionTypes {
     InitializeCurrentProduct = 'Initialize_Current_Product',
     LoadProduct = 'Load_Product',
     LoadProductSuccess = 'Load_Product_Success',
-    LoadProductFail = 'Load_Product_Fail'
+    LoadProductFail = 'Load_Product_Fail',
+    UpdateProduct = 'Update_Product',
+    UpdateProductSuccess = 'Update_Product_Success',
+    UpdateProductFail = 'Update_Product_Fail'
 }
 export class ToggleProductCode implements Action {
     readonly type = ProductActionTypes.ToggleProductCode;
@@ -35,6 +38,18 @@ export class LoadProductFail implements Action {
     readonly type = ProductActionTypes.LoadProductFail;
     constructor(public payload: string) {}
 }
+export class UpdateProduct implements Action {
+    readonly type = ProductActionTypes.UpdateProduct;
+    constructor(public payload: Product) {}
+}
+export class UpdateProductSuccess implements Action {
+    readonly type = ProductActionTypes.UpdateProductSuccess;
+    constructor(public payload: Product) {}
+}
+export class UpdateProductFail implements Action {
+    readonly type = ProductActionTypes.UpdateProductFail;
+    constructor(public payload: string) {}
+}
 export class InitializeCurrentProduct implements Action {
     readonly type = ProductActionTypes.InitializeCurrentProduct;
 }
@@ -45,4 +60,7 @@ export type ProductAction = ToggleProductCode
 | InitializeCurrentProduct
 | LoadProduct
 | LoadProductSuccess
-| LoadProductFail;
+| LoadProductFail
+| UpdateProduct
+| UpdateProductSuccess
+| UpdateProductFail;
